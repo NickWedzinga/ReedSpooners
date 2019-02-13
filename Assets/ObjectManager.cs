f56535bd0f7fd08059e66bc3deac39b74a49105d
NickWedzinga <oxay@live.com>
2019-02-13 10:23:00 +0100

    big boi changes

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -8,18 +8,30 @@ public class ObjectManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Spike = new GameObject("Spike");
+        Spike = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Spike.transform.position = new Vector3(4.75f, 1, 20);
         Spike.AddComponent<Rigidbody>();
-        Spike.AddComponent<MeshFilter>();
         Spike.AddComponent<BoxCollider>();
-        Spike.AddComponent<MeshRenderer>();
+        //Spike = new GameObject("Spike");
+        //Spike.AddComponent<Rigidbody>();
+        //Spike.AddComponent<MeshFilter>();
+        //Spike.AddComponent<BoxCollider>();
+        //Spike.AddComponent<MeshRenderer>();
         //Spike.GetComponent<MeshFilter>().mesh = Mesh.
-        Spike.transform.position = new Vector3(0, 0, 10);
+        //Spike.transform.position = new Vector3(0, 0, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
     }
 }
