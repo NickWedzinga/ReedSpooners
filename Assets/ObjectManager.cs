a9f22e5f1557b0fa259b964ba28f4bdbd9cd0dad
NickWedzinga <oxay@live.com>
2019-02-15 13:42:41 +0100

    Now correctly resets, randomizes subset order and half the variables implemented

--- Assets/ObjectManager.cs
+++ Assets/ObjectManager.cs
@@ -8,19 +8,29 @@ public class ObjectManager : MonoBehaviour
     public GameObject[] Objects = new GameObject[100];
     public int lastObjectPositionZ = 0;
     private int nrOfHighlightedObjects = 10;
+    private Color _OriginalObjectColor;
 
     public Text Announcer;
     private float _AnnouncerTextTimer;
-    private Color _OriginalColor;
+    private Color _OriginalTextColor;
 
     public int VisualVariableCounter;
-        
+    public int highlightType;
+
+    public int[] VisualVariableOrder = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+    private System.Random _random = new System.Random();
+
     // Start is called before the first frame update
     void Start()
     {
-        VisualVariableCounter = 1;
+        //RANDOMIZE VISUALORDER
+        Shuffle(VisualVariableOrder); 
 
-        _OriginalColor = Announcer.color;
+        VisualVariableCounter = 0;
+
+        _OriginalObjectColor = Color.gray;//Objects[0].GetComponent<MeshRenderer>().material.color;
+        _OriginalTextColor = Announcer.color;
         _AnnouncerTextTimer = Time.deltaTime;
         
         Announcer.text = "GET READY FOR ROUND " + VisualVariableCounter.ToString();
@@ -32,10 +42,9 @@ public class ObjectManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(Announcer.IsActive());
         if (_AnnouncerTextTimer > 0)
         {
-            Announcer.color = Color.Lerp(_OriginalColor, Color.clear, Mathf.Min(1, _AnnouncerTextTimer / 3.0f));
+            Announcer.color = Color.Lerp(_OriginalTextColor, Color.clear, Mathf.Min(1, _AnnouncerTextTimer / 3.0f));
             _AnnouncerTextTimer += Time.deltaTime;
             if (_AnnouncerTextTimer > 3.0f)
             {
@@ -53,22 +62,22 @@ public class ObjectManager : MonoBehaviour
             Objects[i].gameObject.name = "Spike";
         }
 
-        RandomHighlight();
+        RandomizeObjectsForHighlight();
 
         // Spawn random objects
         for (int i = 0; i < Objects.Length; ++i)
         {
-            //Objects[i-3] = GameObject.CreatePrimitive(PrimitiveType.Cube);
             Objects[i].AddComponent<Rigidbody>();
             Objects[i].AddComponent<BoxCollider>();
 
             // i* 10 for 10 distance between objects, + i/10 to increase distance between slightly the higher the approach rate, +30 to start at z = 30
-            Objects[i].transform.position = new Vector3(4.75f * RandomLane(), 1, (i) * 10 + i / 10 + 30);
+            Objects[i].transform.position = new Vector3(4.75f * RandomLane(), 1, (i) * 10 + i / 10 + 50);
 
             if (Objects[i].gameObject.name == "Coin")
             {
                 // Object should be highlighted, apply highlight
-                ApplyHighlight(i, 0);
+                //ApplyHighlight(i, VisualVariableOrder[VisualVariableCounter]);
+                ApplyHighlight(i, 3);
             }
             if (i == Objects.Length - 1)
                 lastObjectPositionZ = (int)Objects[i].transform.position.z;
@@ -87,7 +96,7 @@ public class ObjectManager : MonoBehaviour
             return 1;
     }
 
-    void RandomHighlight()
+    void RandomizeObjectsForHighlight()
     {
         List<int> indexList = new List<int>();
 
@@ -106,9 +115,24 @@ public class ObjectManager : MonoBehaviour
         }
     }
 
+    void Shuffle(int[] array)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            // find random index to swap
+            int randomIndex = _random.Next(0, i);
+            int temp = array[randomIndex];
+
+            // swap
+            array[randomIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
     public void Reset()
     {
-        //print("WHERE WE DROPPIN BOIS");
+        VisualVariableCounter++;
+
         for (int i = 0; i < Objects.Length; ++i)
         {
             Objects[i].gameObject.name = "Spike";
@@ -118,32 +142,58 @@ public class ObjectManager : MonoBehaviour
         }
 
         // Choose random objects that will be highlighted
-        RandomHighlight();
+        RandomizeObjectsForHighlight();
 
         // Announcer text reset and active
-        VisualVariableCounter++;
         Announcer.text = "GET READY FOR ROUND " + VisualVariableCounter.ToString();
         Announcer.gameObject.SetActive(true);
         _AnnouncerTextTimer += Time.deltaTime;
-
-        int highlightType = 1;
 
         for (int i = 0; i < Objects.Length; ++i)
         {
             if (Objects[i].gameObject.name == "Coin")
             {
                 // Object should be highlighted, apply highlight
-                ApplyHighlight(i, highlightType);
+                ApplyHighlight(i, VisualVariableOrder[VisualVariableCounter]);
             }
         }
     }
 
     public virtual void ApplyHighlight(int index, int highlightType)
     {
-        // Red Hue
-        if(highlightType == 0)
-            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0);
+        // V0 : Control
+        if (highlightType == 0)
+            Objects[index].GetComponent<MeshRenderer>().material.color = _OriginalObjectColor;
+        // V1 : Hue (red)
         else if (highlightType == 1)
+            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(255, 0, 0);
+        // V2 : Hue (blue)
+        else if (highlightType == 2)
             Objects[index].GetComponent<MeshRenderer>().material.color = new Color(0, 0, 255);
+        // V3 : Motion
+        else if (highlightType == 3)
+            StartCoroutine(Rotator(index));
+        // V4 : Luminance, low value
+        else if (highlightType == 4)
+            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0);
+        // V5 : Luminance, high value
+        else if (highlightType == 5)
+            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(255, 255, 255);
+        else if (highlightType == 6)
+            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(125, 125, 125);
+        else if (highlightType == 7)
+            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(125, 125, 125);
+        else if (highlightType == 8)
+            Objects[index].GetComponent<MeshRenderer>().material.color = new Color(125, 125, 125);
+    }
+
+    private IEnumerator Rotator(int index)
+    {
+        while (VisualVariableOrder[VisualVariableCounter] == 3)
+        {
+            Objects[index].transform.Rotate(new Vector3(0, 1, 0), 20.0f);
+            yield return null;
+        }
+        Objects[index].transform.localRotation = Quaternion.identity;
     }
 }
